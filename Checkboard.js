6567139fd3423e92f726700a491d293b7d47ae3c
will0684 <jordan.walcaraz@gmail.com>
2020-01-21 08:05:18 -0500

    Added second challenge

---
+++ Checkboard.js
@@ -0,0 +1,30 @@
+// Create a function that returns true if the two-dimensional n x n 
+// input array has a checker-board pattern.
+
+// isCheckerboard([
+//   [1, 1],
+//   [0, 1]
+// ]) ➞ false
+
+// isCheckerboard([
+//   [0, 1],
+//   [1, 0]
+// ]) ➞ true
+
+// isCheckerboard([
+//   [1, 0, 1, 0, 1],
+//   [0, 1, 0, 1, 0],
+//   [1, 0, 1, 0, 1],
+//   [0, 1, 0, 1, 0],
+//   [1, 0, 1, 1, 1]
+// ]) ➞ false
+
+// isCheckerboard([
+//   [1, 0, 1, 0, 1],
+//   [0, 1, 0, 1, 0],
+//   [1, 0, 1, 0, 1],
+//   [0, 1, 0, 1, 0],
+//   [1, 0, 1, 0, 1]
+// ]) ➞ true
+
+//------------------------------------------------------------//
